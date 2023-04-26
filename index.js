82119050629c35093ae633dc84b6e07149ecde13
Ethan Daniel <e.daniel.small@gmail.com>
2023-04-26 11:56:40 -0700

    prompts functional

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
 console.log("present!")
+
+inquirer.prompt([
+    {
+        type: 'input',
+        message: 'characters',
+        name: 'please enter up to three characters'
+    },
+    {
+        type: 'list',
+        message: "please choose your logo's color",
+        name: 'inputTest'
+    },
+    {
+        type: 'list',
+        message: "please choose your logo's shape",
+        name: 'inputTest'
+    },
+]
+)
